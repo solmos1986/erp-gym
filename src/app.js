d45cd55d1517cd3097b7f2aaf8943a51a5de1193
Sergio OLMOS <aplus.scz@gmail.com>
2026-04-19 22:50:55 -0400

    modificando app.js CORS

--- src/app.js
+++ src/app.js
@@ -21,13 +21,18 @@ import { WebSocketServer } from 'ws';
 import { sendCommandToAgent } from './lib/websocket.server.js';
 
 const app = express();
-startMembershipExpirationJob();
+
 // =============================
 // 🔥 MIDDLEWARES GLOBALES
 // =============================
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://gymcloud.aplus-security.com', // Asegúrate de que esta sea la URL de tu frontend
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
 app.use(express.json());
-
+startMembershipExpirationJob();
 // 🔥🔥🔥 IMPORTANTE (SERVIR IMÁGENES)
 app.use('/uploads', express.static('uploads'));
 
